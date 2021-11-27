8e8c7e220b4427a256657bf013eb67b63dc6df1d
DuetoPark <inhye.park.dev@gmail.com>
2021-11-27 19:24:21 +0900

    🚧 Separate user-gallery: mobile / desktop

--- js/_carousels.js
+++ js/_carousels.js
@@ -11,11 +11,11 @@ const productCarousel = tns({
   preventScrollOnTouch: true,
 })
 
-const userGallery = tns({
-  container: '.user-gallery .slider-list',
+const userGalleryMobile = tns({
+  container: '.user-gallery.is-mobile .slider-list',
   controls: false,
-  controlsContainer: '.user-gallery .user-gallery-controls',
-  navContainer: '.user-gallery .thumbnail-list',
+  controlsContainer: '.user-gallery.is-mobile .user-gallery-controls',
+  navContainer: '.user-gallery.is-mobile .thumbnail-list',
   navAsThumbnails: true,
   gutter: 4,
   edgePadding: 16,
@@ -23,11 +23,18 @@ const userGallery = tns({
   arrowKeys: true,
   mouseDrag: true,
   preventScrollOnTouch: true,
-  responsive: {
-    768: {
-      gutter: 6,
-      controls: true,
-      edgePadding: 75,
-    },
-  },
+})
+
+const userGalleryDesktop = tns({
+  container: '.user-gallery.is-desktop .slider-list',
+  controlsContainer: '.user-gallery.is-desktop .user-gallery-controls',
+  navContainer: '.user-gallery.is-desktop .thumbnail-list',
+  controls: true,
+  navAsThumbnails: true,
+  gutter: 6,
+  edgePadding: 75,
+  loop: false,
+  arrowKeys: true,
+  mouseDrag: true,
+  preventScrollOnTouch: true,
 })
