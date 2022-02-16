8d48afb397de833454ded5b1cec3247bc86de85b
DuetoPark <inhye.park.dev@gmail.com>
2022-02-16 17:29:12 +0900

    🚧 Implement deleting individual .search-history-item

--- js/gnb-search.js
+++ js/gnb-search.js
@@ -6,18 +6,19 @@ const gnbSearchHistoryList = gnbSearchHistory.querySelector('ol');
 const clearButton = gnbSearchHistory.querySelector(
   '.search-history-header button'
 );
+const deleteButton = gnbSearchHistoryList.querySelector('.delete-button');
+
+function closeGnbSearchHistory() {
+  gnbSearchHistory.classList.remove('is-active');
+  window.removeEventListener('click', closeGnbSearchHistoryOnClickingOutside);
+}
 
 function closeGnbSearchHistoryOnClickingOutside(event) {
-  const target = event.target;
-  if (!gnbSearch.contains(target)) {
-    gnbSearchHistory.classList.remove('is-active');
-    window.removeEventListener('click', closeGnbSearchHistoryOnClickingOutside);
-  }
+  gnbSearch.contains(event.target) || closeGnbSearchHistory();
 }
 
 function openGnbSearchHistory() {
-  if (!gnbSearchHistoryList.innerHTML) return;
-  // NOTE: 조건부에는 gnbSearchHistoryList.children.length가 들어와도 된다.
+  if (!gnbSearchHistoryList.children.length) return;
 
   if (!gnbSearchHistory.matches('.is-active')) {
     window.addEventListener('click', closeGnbSearchHistoryOnClickingOutside);
@@ -25,11 +26,28 @@ function openGnbSearchHistory() {
   gnbSearchHistory.classList.add('is-active');
 }
 
-function clearGnbSearchHistoryList() {
-  gnbSearchHistoryList.innerHTML = '';
-  gnbSearchHistory.classList.remove('is-active');
-}
-
 gnbSearchInput.addEventListener('focus', openGnbSearchHistory);
 
+function clearGnbSearchHistoryList() {
+  gnbSearchHistoryList.innerHTML = '';
+  closeGnbSearchHistory();
+}
+
 clearButton.addEventListener('click', clearGnbSearchHistoryList);
+
+function deleteSearchHistoryItem(event) {
+  event.stopPropagation();
+  // NOTE: event.stopPropagation를 쓴 이유
+  // closeGnbSearchHistoryOnClickingOutside 함수는 대상이 불특정하기 때문에
+  // 이벤트 전파를 막을 때 쓰는 if(e.currentTarget !== e.target) return;은 오히려 동작을 방해함
+
+  const target = event.target;
+  if (!target.matches('.delete-button')) return;
+
+  const itemToDelete = target.parentNode;
+  this.removeChild(itemToDelete);
+
+  gnbSearchHistoryList.children.length || closeGnbSearchHistory();
+}
+
+gnbSearchHistoryList.addEventListener('click', deleteSearchHistoryItem);
