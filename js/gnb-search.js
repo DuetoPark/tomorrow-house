4d219b332328dfffae0e6de1d83bf89fe8583004
DuetoPark <inhye.park.dev@gmail.com>
2022-02-16 16:11:35 +0900

    🚧 Implement clearing .search-history-list

--- js/gnb-search.js
+++ js/gnb-search.js
@@ -1,6 +1,11 @@
 const gnbSearch = document.querySelector('.gnb-search');
-const gnbSearchHistory = gnbSearch.querySelector('.search-history');
 const gnbSearchInput = gnbSearch.querySelector('.form-input');
+const gnbSearchHistory = gnbSearch.querySelector('.search-history');
+const gnbSearchHistoryList = gnbSearchHistory.querySelector('ol');
+
+const clearButton = gnbSearchHistory.querySelector(
+  '.search-history-header button'
+);
 
 function closeGnbSearchHistoryOnClickingOutside(event) {
   const target = event.target;
@@ -11,10 +16,20 @@ function closeGnbSearchHistoryOnClickingOutside(event) {
 }
 
 function openGnbSearchHistory() {
+  if (!gnbSearchHistoryList.innerHTML) return;
+  // NOTE: 조건부에는 gnbSearchHistoryList.children.length가 들어와도 된다.
+
   if (!gnbSearchHistory.matches('.is-active')) {
     window.addEventListener('click', closeGnbSearchHistoryOnClickingOutside);
   }
   gnbSearchHistory.classList.add('is-active');
 }
 
+function clearGnbSearchHistoryList() {
+  gnbSearchHistoryList.innerHTML = '';
+  gnbSearchHistory.classList.remove('is-active');
+}
+
 gnbSearchInput.addEventListener('focus', openGnbSearchHistory);
+
+clearButton.addEventListener('click', clearGnbSearchHistoryList);
