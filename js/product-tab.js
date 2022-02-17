03db883d69b6f0b3d2ab5eb8b62ba2daf52ca142
DuetoPark <inhye.park.dev@gmail.com>
2022-02-17 21:26:55 +0900

    💫 Scroll to tab-panel when .product-tab-list is clicked

--- js/product-tab.js
+++ js/product-tab.js
@@ -1,6 +1,10 @@
 const productTabList = document.querySelector('.product-tab-list');
 const productTabItemList = productTabList.querySelectorAll('li');
 
+const TOP_HEADER_MOBILE = 50 + 40 + 40;
+const TOP_HEADER_DESKTOP = 80 + 50 + 54;
+const BREAKPOINT_DESKTOP = 768;
+
 function activeTabItem(event) {
   const target = event.target;
   const tabItem = target.parentNode;
@@ -14,4 +18,27 @@ function activeTabItem(event) {
   tabItem.classList.toggle('is-active');
 }
 
+function scrollToTabPanel(event) {
+  const target = event.target;
+  const tabItem = target.parentNode;
+
+  const id = tabItem.getAttribute('aria-labelledby');
+  const tabPanel = document.querySelector(`#${id}`);
+
+  const tabPanelTop = tabPanel.offsetTop;
+  const scrollAmount =
+    tabPanelTop -
+    (window.innerWidth >= BREAKPOINT_DESKTOP
+      ? TOP_HEADER_DESKTOP
+      : TOP_HEADER_MOBILE);
+
+  window.scrollTo({
+    top: scrollAmount,
+    behavior: 'smooth',
+  });
+  // scrollBy를 쓰려면,
+  // tapPanel의 top => tabPanel.getBoundingClientRect().top으로 수정한다.
+}
+
 productTabList.addEventListener('click', activeTabItem);
+productTabList.addEventListener('click', scrollToTabPanel);
