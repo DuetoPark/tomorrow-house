8ca9b39696269261f823b60339691101ca16a1f3
DuetoPark <inhye.park.dev@gmail.com>
2022-02-17 21:22:26 +0900

    🚧 Reimplement toggling selected tab item

--- js/product-tab.js
+++ js/product-tab.js
@@ -1,32 +1,17 @@
 const productTabList = document.querySelector('.product-tab-list');
 const productTabItemList = productTabList.querySelectorAll('li');
 
-function getElementHeight(className) {
-  const element = document.querySelector(className);
-  const elementRect = element.getBoundingClientRect();
-  const elementHeight = elementRect.height;
-
-  return elementHeight;
-}
-
-function InactiveAllTabItems() {
-  productTabItemList.forEach((item) => {
-    item.classList.remove('is-active');
-  });
-}
-
-function makeSelectedTabItemActive(tabItem) {
-  InactiveAllTabItems();
-  tabItem.classList.toggle('is-active');
-}
-
-function showMatchedSection(event) {
+function activeTabItem(event) {
   const target = event.target;
   const tabItem = target.parentNode;
 
   if (tabItem.matches('.is-active')) return;
 
-  makeSelectedTabItemActive(tabItem);
+  productTabItemList.forEach((item) => {
+    item.classList.remove('is-active');
+  });
+
+  tabItem.classList.toggle('is-active');
 }
 
-productTabList.addEventListener('click', showMatchedSection);
+productTabList.addEventListener('click', activeTabItem);
